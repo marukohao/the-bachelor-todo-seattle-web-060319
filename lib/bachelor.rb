68b5d242c79157da0ee61c44664b1d5ecbcaf25a
hao wang <ge_nah@hotmail.com>
2019-05-14 22:03:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,6 +13,17 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = nil 
+  data.each do |season, person|
+    person.each do |hash|
+      hash.each do |key, value|
+        if value == occupation
+          name = hash["name"]
+        end
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
