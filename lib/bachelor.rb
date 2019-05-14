1f0c6bcd11aef38ddee6d7f5307f74ddea41bdb2
hao wang <ge_nah@hotmail.com>
2019-05-14 22:00:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,14 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  name = nil 
   data[season].each do |hash|
+    hash.each do |key, value|
+      if value == "Winner"
+        name = hash["name"]  
+      end
+    end
+  end
+  name.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
